96fa02b67102165fad817477e7e53e7ceebbb5ee
Denis Tislenko <denis.tislenko@readsoft.com>
2018-01-11 10:51:21 +0100

    hashRouter added

--- web/index.jsx
+++ web/index.jsx
@@ -3,7 +3,7 @@
 // Import React base modules
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Switch, HashRouter as BrowserRouter, Route } from 'react-router-dom';
+import { Switch, HashRouter as AppRouter, Route } from 'react-router-dom';
 //eslint-disable-next-line
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
@@ -15,7 +15,7 @@ import MapNavigation from './components/MapNavigation.jsx';
 import TalkDialog from './components/TalkDialog.jsx';
 
 ReactDom.render(
-  <BrowserRouter>
+  <AppRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/menu" component={Menu} />
@@ -23,7 +23,10 @@ ReactDom.render(
       <Route path="/talk" component={TalkDialog} />
       <Route path="*" component={NotFound} />
     </Switch>
-  </BrowserRouter>,
+  </AppRouter>,
+  //BrowserRouter is used for pages with dynamic addresses (like search engines)
+  //HashRouter - for static web pages. So far I have static web pages
+  //Here is how redirect can be done in case user still needs to login/etc: https://stackoverflow.com/questions/38279555/auth-based-redirecting-with-react-router
   // Here is how query can be hidden: https://stackoverflow.com/questions/42089626/how-to-hide-query-string-parameter-in-reactjs-react-router
   // Check this code in Navigation object it if will be possible to hide final url
   //   .makePath('about') // return URL
